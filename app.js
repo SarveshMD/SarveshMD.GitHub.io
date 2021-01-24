3bdb49d5111cf21f382b6f62518d4625a6d2af27
Sarvesh <mdsarvesh1013@gmail.com>
2021-01-24 22:16:33 +0530

    Used some prettify package to prettify code of all files. No other changes made.

--- app.js
+++ app.js
@@ -1,103 +1,102 @@
 if (screen.width <= 800) {
-	var err_params = {
-	status_code :  401, 
-	message :  "Please try again with a device with bigger screen.", 
-	hideButton  : true }
-	var rdurl = `err/?${jQuery.param(err_params)}`
-  location.replace(rdurl);
+    var err_params = {
+        status_code: 401,
+        message: "Please try again with a device with bigger screen.",
+        hideButton: true
+    }
+    var rdurl = `err/?${jQuery.param(err_params)}`
+    location.replace(rdurl);
 }
 
 startup()
 showLoading(0)
 
 
-function showLoading(secs){
-	console.log(secs)
-	document.getElementById("loader").style.visibility = "visible";
-	document.getElementById("full").style.visibility = "hidden";
-	setTimeout(function () {
-		document.getElementById("loader").style.visibility = "hidden";
-	}, secs+0.2*1000)
-	setTimeout(function() {
-		document.getElementById("full").style.visibility = "visible";
-	}, secs+0.5*1000)
+function showLoading(secs) {
+    console.log(secs)
+    document.getElementById("loader").style.visibility = "visible";
+    document.getElementById("full").style.visibility = "hidden";
+    setTimeout(function() {
+        document.getElementById("loader").style.visibility = "hidden";
+    }, secs + 0.2 * 1000)
+    setTimeout(function() {
+        document.getElementById("full").style.visibility = "visible";
+    }, secs + 0.5 * 1000)
 }
 
 
 async function startup() {
-	var urlParams = new URLSearchParams(window.location.search);
-	var send_date = urlParams.get("d");
+    var urlParams = new URLSearchParams(window.location.search);
+    var send_date = urlParams.get("d");
 
-	if (send_date == null || send_date == "") {
-		var jsDate = new Date();
-	    var dd = String(jsDate.getDate()).padStart(2, "0");
-	    var mm = String(jsDate.getMonth() + 1).padStart(2, "0");
-	    var yyyy = String(jsDate.getFullYear());
-	    var send_date = `${yyyy}-${mm}-${dd}`;
+    if (send_date == null || send_date == "") {
+        var jsDate = new Date();
+        var dd = String(jsDate.getDate()).padStart(2, "0");
+        var mm = String(jsDate.getMonth() + 1).padStart(2, "0");
+        var yyyy = String(jsDate.getFullYear());
+        var send_date = `${yyyy}-${mm}-${dd}`;
         var read_date = `${dd}-${mm}-${yyyy}`;
-		var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
-		if (response.status != 200) {
-			var jsDate = new Date();
-		    var dd = String(jsDate.getDate() - 1).padStart(2, "0");
-		    var mm = String(jsDate.getMonth() + 1).padStart(2, "0");
-		    var yyyy = String(jsDate.getFullYear());
-		    var send_date = `${yyyy}-${mm}-${dd}`;
-		    var read_date = `${dd}-${mm}-${yyyy}`;
-			var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
-		}
-	}
-	else {
-		var send_date = urlParams.get("d");
-	    var dd = send_date.slice(8);
-	    var mm = send_date.slice(5, 7);
-	    var yyyy = send_date.slice(0, 4);
-	    var read_date = `${dd}-${mm}-${yyyy}`;
-		var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
-	}
-	var vars = await response.json()
-	if (!response.ok || response.status != 200 || response.statusText != "OK") {
-		var params = {};
-		params.status_code = response.status;
-		params.status_text = response.statusText;
-		params.message  = vars.msg;
-		var redirect_url = `err/?${jQuery.param(params)}`;
-		location.replace(redirect_url);
-	}
-	if (vars.media_type != "video") {
-		document.querySelector("#main_image").src = vars.hdurl;
-		document.querySelector("#load_img").src = "imgs/loading.gif";		
-		document.querySelector("#main_image").onload = function() {
-			document.querySelector("#load_img").style.display = "none";
-			document.querySelector("#waiting").style.display = "none";
-			document.querySelector("#showHalf").style.display = "none";
-			document.querySelector("#main_image").style.display = "flex";
-			document.querySelector("#main_image").src = vars.hdurl;
-		};
-		document.querySelector("#image_anchor").href = vars.hdurl;
-		document.querySelector("#video_container").style.display = "none";
-	}
-	else {
-		document.querySelector("#load_img").style.display = "none";
-		document.querySelector("#waiting").style.display = "none";
-		document.querySelector("#showHalf").style.display = "none";
-		document.getElementById("image_anchor").style.display = "none";
-		document.getElementById("main_image").style.display = "none";
-		document.getElementById("video_container").innerHTML = 
-		`<iframe width="1366" height="625" src="${vars.url}" allowfullscreen></iframe>`;
-	}
-	document.getElementById("date").innerHTML = read_date;
-	document.getElementById("img_title").innerHTML = vars.title;
-	document.getElementById("explanation").innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${vars.explanation}`;
-	var cpright = vars.copyright;
-	if (cpright == undefined) {
-		cpright = "National Aeronautics and Space Administration (NASA)";
-	}
-	document.getElementById("copyright").innerHTML = `${yyyy} ${cpright}`;
+        var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
+        if (response.status != 200) {
+            var jsDate = new Date();
+            var dd = String(jsDate.getDate() - 1).padStart(2, "0");
+            var mm = String(jsDate.getMonth() + 1).padStart(2, "0");
+            var yyyy = String(jsDate.getFullYear());
+            var send_date = `${yyyy}-${mm}-${dd}`;
+            var read_date = `${dd}-${mm}-${yyyy}`;
+            var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
+        }
+    } else {
+        var send_date = urlParams.get("d");
+        var dd = send_date.slice(8);
+        var mm = send_date.slice(5, 7);
+        var yyyy = send_date.slice(0, 4);
+        var read_date = `${dd}-${mm}-${yyyy}`;
+        var response = await fetch(`https://api.nasa.gov/planetary/apod?thumbs=true&api_key=FW3Yre1Qb0KJCuI5xLDspAOS3oG4ddA19j7qZmsA&date=${send_date}`);
+    }
+    var vars = await response.json()
+    if (!response.ok || response.status != 200 || response.statusText != "OK") {
+        var params = {};
+        params.status_code = response.status;
+        params.status_text = response.statusText;
+        params.message = vars.msg;
+        var redirect_url = `err/?${jQuery.param(params)}`;
+        location.replace(redirect_url);
+    }
+    if (vars.media_type != "video") {
+        document.querySelector("#main_image").src = vars.hdurl;
+        document.querySelector("#load_img").src = "imgs/loading.gif";
+        document.querySelector("#main_image").onload = function() {
+            document.querySelector("#load_img").style.display = "none";
+            document.querySelector("#waiting").style.display = "none";
+            document.querySelector("#showHalf").style.display = "none";
+            document.querySelector("#main_image").style.display = "flex";
+            document.querySelector("#main_image").src = vars.hdurl;
+        };
+        document.querySelector("#image_anchor").href = vars.hdurl;
+        document.querySelector("#video_container").style.display = "none";
+    } else {
+        document.querySelector("#load_img").style.display = "none";
+        document.querySelector("#waiting").style.display = "none";
+        document.querySelector("#showHalf").style.display = "none";
+        document.getElementById("image_anchor").style.display = "none";
+        document.getElementById("main_image").style.display = "none";
+        document.getElementById("video_container").innerHTML =
+            `<iframe width="1366" height="625" src="${vars.url}" allowfullscreen></iframe>`;
+    }
+    document.getElementById("date").innerHTML = read_date;
+    document.getElementById("img_title").innerHTML = vars.title;
+    document.getElementById("explanation").innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${vars.explanation}`;
+    var cpright = vars.copyright;
+    if (cpright == undefined) {
+        cpright = "National Aeronautics and Space Administration (NASA)";
+    }
+    document.getElementById("copyright").innerHTML = `${yyyy} ${cpright}`;
 }
 
 
 document.querySelector("#showHalf").addEventListener("click", function() {
-	document.getElementById("main_image").style.display = "inline-block";
-	document.getElementById("showHalf").style.display = "none";
-	document.getElementById("load_img").style.display = "none";
+    document.getElementById("main_image").style.display = "inline-block";
+    document.getElementById("showHalf").style.display = "none";
+    document.getElementById("load_img").style.display = "none";
 })
--- err/app.js
+++ err/app.js
@@ -1,47 +1,46 @@
-
 var errors = {
-	"400" : "Bad Request",
-	"401" : "Unauthorized",
-	"403" : "Forbidden",
-	"404" : "Not Found",
-	"429" : "Too Many Requests",
-	"500" : "Internal Server Error",
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "403": "Forbidden",
+    "404": "Not Found",
+    "429": "Too Many Requests",
+    "500": "Internal Server Error",
 };
 var inrHTML = "";
 var additional_message;
 var urlParams = new URLSearchParams(window.location.search);
 var status_code = urlParams.get("status_code");
 var status_text = errors[status_code]
-var hideButton  = urlParams.get("hideButton")
+var hideButton = urlParams.get("hideButton")
 var message = urlParams.get("message");
 
 if (status_text == undefined) {
-	var status_text = urlParams.get("status_text");
+    var status_text = urlParams.get("status_text");
 }
 
 if (message == undefined || message == "") {
-	message = "Your request could not be satisfied"
+    message = "Your request could not be satisfied"
 }
 
 if (status_code == "429") {
-	message = "You have reached the maximum requests you can make per hour."
-	var additional_message = " Please try again in an hour."
+    message = "You have reached the maximum requests you can make per hour."
+    var additional_message = " Please try again in an hour."
 }
 
 inrHTML += "<h1>Aww, That's an error !<h1>";
 if (status_text == null || status_text == "") {
-	var status_text = errors['status_code'];
-	if (status_text == undefined) {
-		status_code = "500"
-		status_text = "Unknown Error"
-	}
-	}
+    var status_text = errors['status_code'];
+    if (status_text == undefined) {
+        status_code = "500"
+        status_text = "Unknown Error"
+    }
+}
 inrHTML += `<h2>${status_code} ${status_text}</h2>`
 inrHTML += `<h2>${message}</h2>`;
 
 if (additional_message != undefined) {
-	inrHTML += `<h2>${additional_message}</h2>`
-	console.log(additional_message)
+    inrHTML += `<h2>${additional_message}</h2>`
+    console.log(additional_message)
 }
 
 inrHTML += `<button id="back">Back</button>`;
@@ -49,8 +48,8 @@ inrHTML += `<button id="back">Back</button>`;
 document.getElementById("content").innerHTML = inrHTML;
 
 document.getElementById("back").addEventListener("click", function() {
-	location.replace("/")
+    location.replace("/")
 })
 if (hideButton == "true") {
-	document.getElementById("back").style.display = "none";
+    document.getElementById("back").style.display = "none";
 }
